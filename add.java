2aa898b70a5b9873dd0fccf5f86b24d655ef1c3c
Arpanchauhan26 <arpanchauhan226@gmail.com>
2023-07-12 10:58:39 +0530

    first Commit

---
+++ add.java
@@ -0,0 +1,22 @@
+public class add {
+        public static void main(String[] args) {
+            int a = 54;
+            int b = 85;
+            float c = a+b;
+            System.out.println(c);
+            int f=5,g=6,h=f+g;
+            System.out.println(h);
+            byte j=5;
+            System.out.println(j);
+            short k=557;
+            System.out.println(k);
+            long l=697455462644517L;
+            System.out.println(l);
+            float s=j+k;
+            System.out.println(s);
+            double aa =f+g/ g-h;
+            System.out.println(aa);
+        }
+    }
+    
+
